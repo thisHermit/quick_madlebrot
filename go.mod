5ffbd89d85c60ed4708b0fe210034ee8b2fdc72e
Bashar Jaan Khan <basharjaankhan@gmail.com>
2025-04-04 19:04:48 +0200

    feat: add proper colors

--- go.mod
+++ go.mod
@@ -2,8 +2,19 @@ module quick_mandlebrot
 
 go 1.24.1
 
+require github.com/fogleman/gg v1.3.0
+
 require (
-	github.com/fogleman/gg v1.3.0 // indirect
+	github.com/mazznoer/csscolorparser v0.1.5 // indirect
+	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+	golang.org/x/term v0.28.0 // indirect
+)
+
+require (
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	github.com/mazznoer/colorgrad v0.10.0
+	github.com/schollz/progressbar/v3 v3.18.0
 	golang.org/x/image v0.25.0 // indirect
 )
--- main.go
+++ main.go
@@ -5,51 +5,56 @@ import (
 	"math/cmplx"
 
 	"github.com/fogleman/gg"
+	"github.com/mazznoer/colorgrad"
+	"github.com/schollz/progressbar/v3"
 )
 
 func main() {
-	dc := gg.NewContext(1000, 1000)
-	dc.DrawCircle(500, 500, 400)
-	dc.SetRGB(0.5, 1, 1)
-	dc.Fill()
-	dc.SavePNG("out.png")
 	mandle_brot()
 	// square()
 	// test_dots()
-	// println(base_fractal_func(0, 2, complex(+9.800000e-001, +0.800000e-001)))
+	// println(base_fractal_func(0, 2, complex(+9.800000e-001, +9.800000e-001)))
 }
 
 func mandle_brot() {
-	x_offset := -1.2
+	x_offset := -1.5
 	max_lim := 1000.0
 	lower_lim := -2 * max_lim
 	upper_lim := 2 * max_lim
 	granularity := 10.0
-	scaling := granularity / 4 // 100 => 25.0
+	scaling := granularity / 2 // 100 => 25.0
 	color := 0.0
 	points := 0
+	grad, _ := colorgrad.NewGradient().
+		HtmlColors("#000000", "#ffffff", "#4775de").
+		Build()
 
 	dc := gg.NewContext(int(math.Abs(2*max_lim)), int(max_lim))
 	dc.SetRGB(1, 1, 1)
 	dc.Clear()
 	dc.SetRGB(0, 0, 0)
 
+	bar := progressbar.Default(int64(upper_lim * upper_lim))
 	for x := lower_lim; x < upper_lim; x += granularity {
 		for y := lower_lim; y < upper_lim; y += granularity {
 			c := complex(x/max_lim+x_offset, y/max_lim)
 			// println("before x,y=", x, y)
 			draw_x, draw_y := transform_coords(x, y, max_lim, granularity)
 			color = base_fractal_func(0, 2, c)
-			dc.SetRGB(color, color, color)
-			// println("after x,y=", draw_x, draw_y)
-			dc.DrawCircle(draw_x, draw_y, scaling)
-			dc.Fill()
-
+			if color != 1 {
+				color_tuple := grad.At(color)
+				dc.SetRGB(color_tuple.R, color_tuple.G, color_tuple.B)
+				// println("after x,y=", draw_x, draw_y)
+				dc.DrawRectangle(draw_x, draw_y, scaling, scaling)
+				dc.Fill()
+			}
 			points++
 			// println("color=", color)
 			// println("points=", points)
+			bar.Add(1)
 		}
 	}
+	println("points=", points)
 	dc.Fill()
 	dc.SavePNG("mandlebrot.png")
 }
@@ -59,13 +64,18 @@ func base_fractal_func(z complex128, exp complex128, c complex128) float64 {
 	// println("z=", z)
 	// println("exp=", exp)
 	// println("c=", c)
-	max_num := 1000
+	epsilon := 1e-4
+
+	max_num := 10000
 	for i := range max_num {
 		z = cmplx.Pow(z, exp) + c
 		// println("z=", z)
 		if real(z) > math.MaxFloat64 || imag(z) > math.MaxFloat64 {
 			// println("broke here z=", z)
 			// println("i=", i)
+			if math.Abs(float64(max_num-i+1)/float64(max_num)) < epsilon {
+				return 1
+			}
 			return float64(max_num-i+1) / float64(max_num)
 		}
 	}
